4e89c4b9449b3eb85af05fc0b6229f343094b495
Sergey.Sirosh <ssv.ruby@gmail.com>
2022-10-07 20:23:08 +0200

    Change ode for users token - delete thread

--- auth.py
+++ auth.py
@@ -1,25 +1,17 @@
 from hh_const import *
-
 import webbrowser
 import socket
-from threading import Thread
 
-
-### Получение авторизации пользователя
-#def w_open():
-#    webbrowser.open_new_tab(USER_AUTH_PARAMS)
-
+# ----------------------------------------
+# Получение кода авторизации пользователя
+# ________________________________________
 def get_code_for_users_token(code_fut=''):
-    def w_open():
-        webbrowser.open_new_tab(USER_AUTH_PARAMS)
 
+    webbrowser.open_new_tab(USER_AUTH_PARAMS)
 
     sock = socket.socket()
     sock.bind((URI, PORT))
     sock.listen(1)
-
-    w_thread = Thread(target=w_open)
-    w_thread.start()
 
     conn, addr = sock.accept()
 
@@ -28,10 +20,9 @@ def get_code_for_users_token(code_fut=''):
         if not data:
             break
         code_fut = data.decode('utf-8').split(' ')[1][7:]
-        conn.close()
-        w_thread._delete()
         break
 
+    conn.close()
     return code_fut
 
 
